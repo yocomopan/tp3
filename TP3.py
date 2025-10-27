d0e080641064169613322d64f6c92cdfeba300ae
zheryakovi <zheryakovi@educ.college-montreal.qc.ca>
2025-10-27 15:26:41 -0400

    Modif.

--- TP3.py
+++ TP3.py
@@ -4,7 +4,13 @@ Ivan Zheryakov
 Combat de monstres
 """
 
+import random
 
-print("hello world!")
+hp = 20
+dice = random.randint(1, 6)
+opposing_power = random.randint(1, 5)
+play_game = True
+options = ["combattre", "eviter et ouvrir une autre portes", "regles du jeu"]
 
-
+while play_game:
+    input("Appuyer pour commencer")

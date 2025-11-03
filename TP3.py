1657d8ea3434cb0a368b8dd5155888eee6a0a9e3
zheryakovi <zheryakovi@educ.college-montreal.qc.ca>
2025-11-03 15:32:42 -0500

    Modif. dice, position changer

--- TP3.py
+++ TP3.py
@@ -7,32 +7,34 @@ Combat de monstres
 import random
 
 hp = 20
-dice = random.randint(1, 6)
-opposing_power = random.randint(1, 6)
-opposing_hp = random.randint(5, 20)
 combat = 0
 combat_win = 0
 combat_loss = 0
 play_game = True
 Game = True
-
+"""
+def options()
+    
+    
+"""
 input("Appuyer pour commencer")
 
 while play_game:
+    opposing_hp = random.randint(5, 20)
+    opposing_power = random.randint(1, 6)
     print(f"Vous tombez face à face avec un adversaire de difficulté : {opposing_power}")
 
     options = int(input("1- combattre\n2- eviter et ouvrir une autre porte\n3- regle du jeu\n4- Quitter le jeu\n"))
-
     if options == 1:
         combat += 1
-        battle_info = input(f"Adversaire: {opposing_hp}\nForce de l’adversaire: {opposing_power}\nNiveau de vie "
-                            f"de l’usager: {hp}\nCombat {combat}: Victoires {combat_win} vs. Defaites\n{
-                            combat_loss}")
-
+        battle_info = input(f"Vie d'adversaire: {opposing_hp}\nForce de l’adversaire: {opposing_power}\nNiveau de "
+                            f"vie de l’usager: {hp}\nCombat {combat}: \nVictoires: {combat_win} vs. Defaites: {
+                            combat_loss}\n")
+        dice = random.randint(1, 6)
     elif options == 2:
         hp -= 1
         print(f"Niveau de vie de l’usager: {hp}")
+
     else:
         print("non")
         play_game = False
-

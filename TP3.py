0075175d6c1cc7b2d0c49cbdec421e82164db392
zheryakovi <zheryakovi@educ.college-montreal.qc.ca>
2025-12-03 16:19:15 -0500

    Modif. + Boss combat, reparer les regles, not showed rules, + hp =+ 4, changer de places quelques lignes de codes

--- TP3.py
+++ TP3.py
@@ -11,6 +11,9 @@ combat = 0
 combat_win = 0
 combat_loss = 0
 play_game = True
+showed_rules = False
+opposing_power = 0
+power = 0
 
 input("Appuyer pour commencer")
 
@@ -21,31 +24,45 @@ while play_game:
               f"Vous avez {combat_win} victoires et {combat_loss} defaites.")
         play_game = False
         continue
-    opposing_power = random.randint(1, 6)
+    if combat_win != 0:
+        if combat_win % 3 == 0:
+            opposing_power = random.randint(3, 5)
+            print("Boss Time")
+    else:
+        if not showed_rules:
+            opposing_power = random.randint(1, 5)
+            power = 20#random.randint(1, 6)
+        else:
+            showed_rules = False
+
     print(f"Vous tombez face à face avec un adversaire de difficulté : {opposing_power}")
     options = int(input("1- combattre\n2- eviter et ouvrir une autre porte\n3- regle du jeu\n4- Quitter le jeu\n"))
 
     if options == 1:
         combat += 1
-        power = random.randint(1, 6)
-        battle_info = input(f"Force de l’adversaire: {opposing_power}\n"
-                            f"Niveau de vie de l’usager: {hp}\n"
-                            f"Combat:  {combat} \n"
-                            f"Victoires: {combat_win} vs. Defaites: {combat_loss}\n")
+
         if opposing_power < power:
+            print(f"roulé(e) un {power}")
             combat_win += 1
-            value = power - opposing_power
-            print(f"Bravo! Vous avez gagner grace à {value} pts de pouvoir de plus que l'ennemi!")
+            hp += opposing_power
+            print(f"Bravo! Vous avez gagner grace à {power - opposing_power} pts de pouvoir de plus que l'ennemi!")
         else:
             combat_loss += 1
+            print(f"roulé(e) un {power}")
             hp -= opposing_power
             print("Vous avez perdu... Meilleure chance la prochaine fois!")
+
+        print(f"Force de l’adversaire: {opposing_power}\n"
+              f"Niveau de vie de l’usager: {hp}\n"
+              f"Combat:  {combat} \n"
+              f"Victoires: {combat_win} vs. Defaites: {combat_loss}\n")
 
     elif options == 2:
         hp -= 1
         print(f"Niveau de vie de l’usager: {hp}")
 
     elif options == 3:
+        showed_rules = True
         print("Vous êtes dans un donjon et vous lancez des dés pour survivre.\n"
               "- Si votre lancer de dé est supérieur à celui de votre ennemi,\n"
               "vous gagnez et passez au donjon suivant.\n"
@@ -54,6 +71,7 @@ while play_game:
               "- Finalement, vous pouvez toujours quitter le jeu quand vous voulez.")
         input("Fermer")
 
+
     elif options == 4:
-        print("Votre Progrès ne sauvegarde pas! Bon soir.")
+        print("Votre progrès ne sauvegarde pas! Bon soir.")
         play_game = False
